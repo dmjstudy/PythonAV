ee9f5207d3d97cbd4be1ab86b126b18586dc67ea
dmjstudy <dmjstudy@gmail.com>
2018-05-30 07:04:57 +0800

    写作业

---
+++ 04func/Atm/core/bak_atm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# coding: utf-8 
+"""
+__title__ = ''
+__author__ = ''dengminjie
+__mtime__ = '18/05/30 0030'
+# 
+"""
+
+def load_db():
+    pass
+
+def auth_user():
+    pass
+
+def view_account_info():
+    pass
+
+def with_draw():
+    #取现
+    pass
+
+def pay_back():
+    #还款
+    pass
+
+def transfer():
+    #转账
+    pass
+
+def  make_transaction():
+    #交易中心
+    pass
--- 04func/Atm/core/main.py
+++ 04func/Atm/core/main.py
@@ -2,3 +2,20 @@ __author__ = "Alex Li"
 
 def login():
     print("Welcome to my atm")
+
+
+def goods():
+    #货物与价格
+    pass
+
+def calc_money():
+    #结账
+    pass
+
+def conn_bak():
+    #连接,
+    pass
+
+def authenticate():
+    #认证
+    pass
