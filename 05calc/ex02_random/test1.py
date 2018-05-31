d5183f52069107df0a74e736aec6c7479e6cfe0f
dmjstudy <dmjstudy@gmail.com>
2018-05-31 16:16:21 +0800

    整理了一个05，添加了14文件夹

--- 05calc/ex02_random/test1.py
+++ 05calc/ex02_random/test1.py
@@ -1,8 +1,8 @@
 __author__ = "Alex Li"
 import random
 checkcode=''
-for i in range(5):
-    current=random.randrange(0,5)
+for i in range(6):
+    current=random.randrange(0,6)
     if current == i:
         tmp=chr(random.randint(65,90))
     else:
